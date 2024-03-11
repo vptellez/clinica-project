9b6b451aef536a32f5739a33f6362dbef5a73eb4
hardev-app <hardev.app@gmail.com>
2024-03-11 13:03:53 -0600

    Mockup Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clinica-dental';
+  title = 'Clinica Dental';
+  showMenu = false;
+  toggleNavbar(){
+    this.showMenu = !this.showMenu;
+  }
 }
